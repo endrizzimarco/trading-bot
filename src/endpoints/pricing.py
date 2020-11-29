8949209135c8bc035f30e1d3c953aac74876f5b9
Marco Endrizzi <marco.endrizzi.bz@gmail.com>
2020-11-29 13:49:54 +0000

    Added Pricing endpoint

---
+++ src/endpoints/pricing.py
@@ -0,0 +1,15 @@
+import oandapyV20.endpoints.pricing as pricing
+from connection import Connection
+
+class Pricing:
+  # Load config
+  conn = Connection.getInstance()
+  accountID = conn.config['ACCOUNT_ID']
+
+  def __init__(self, instrument):
+    self.q = pricing.PricingInfo(self.accountID, {"instruments": instrument})
+    self.data = self.conn.API.request(self.q)['prices'][0]
+    self.unitsAvailable = self.data['unitsAvailable']['default']['long']
+
+  def is_tradeable(self):
+    return True if self.data['tradeable'] == 'True' else False
